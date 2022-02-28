8a4a6ced6c69a64c0bf6b28629e0490f7dca800c
Connor Larkin <connor@connorlarkin.com>
2022-02-28 02:52:05 -0600

    mmm

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import Lottie from "lottie-react";
 
+import wip from './assets/8633-web-development.json'
 import Button from '@mui/material/Button';
 
 
@@ -15,8 +15,9 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          connorlarkin.com is here
+          welcome to connorlarkin.com
         </Typography>
+        <Lottie animationData={wip} />
 
       </Box>
     </Container>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import App from './App';
 
 

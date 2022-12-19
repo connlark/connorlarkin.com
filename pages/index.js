d730b5aefdb09929358acf02a38c3e8d5369e568
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-12-19 16:07:43 -0600

    works

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button, Spacer } from '@nextui-org/react';
+
+
+
+const GH = () => <Button>github</Button>;
+const TWTR = () => <Button>twitter</Button>;
+
+
+
 
 export default function Home() {
   return (
@@ -13,43 +22,20 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          connorlarkin.com
+      </h1>
+      <Spacer/>
+      <Spacer/>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <Button.Group size="xl" vertical color="gradient" bordered>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Button onClick={() => {window.location.replace('https://github.com/connlark')}}>github</Button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <Button onClick={() => {window.location.replace('https://twitter.com/connlark')}}>twitter</Button>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Button onClick={() => {window.location.replace('https://bitbybite.xyz')}}>bitbybite.xyz</Button>
+        
+      </Button.Group>
       </main>
 
       <footer className={styles.footer}>
@@ -58,7 +44,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          connorlarkin.com © 2022
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
